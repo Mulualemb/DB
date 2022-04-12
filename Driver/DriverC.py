d53df5e9e88b34ba702b2dbdb46c13024919b577
Mulualemb <elibogala@gmail.com>
2022-04-12 16:29:56 +0300

    3d driver test

--- Driver/DriverC.py
+++ Driver/DriverC.py
@@ -7,6 +7,25 @@ from Driver.chromedriver import *
 # driver = webdriver.Chrome("..\Driver\chromedriver\chromedriver.exe")
 # driver.get("https://www.python.org")
 
+#facebook forgot password button test.
+# driver = webdriver.Chrome("..\Driver\chromedriver\chromedriver.exe")
+# driver.get("https://www.facebook.com/")
+# driver.find_element_by_xpath("//body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/form[1]/div[3]/a[1]").click()
+
+
+# #facebook create new account button test.
+# driver = webdriver.Chrome("..\Driver\chromedriver\chromedriver.exe")
+# driver.get("https://www.facebook.com/")
+# driver.find_element_by_xpath("//body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/form[1]/div[5]/a[1]").click()
+
+
+
+
+
+
+
+
+
 
 # edge
 # driver = webdriver.Edge("..\Driver\edgedriver\msedgedriver.exe")
@@ -15,15 +34,17 @@ from Driver.chromedriver import *
 
 
 
-# firefox
+# firefox1
 # driver = webdriver.Firefox("\Driver\Firefoxdriver\geckodriver.exe")
 # driver.get("https://www.python.org")
 
 
+# firefox2
 # driver = webdriver.Firefox("..\Driver\Firefoxdriver\geckodriver.exe")
 # driver.get("https://www.python.org")
 
 
+# firefox3
 # driver = webdriver.Firefox(executable_path="..\Driver\Firefoxdriver\geckodriver.exe")
 # driver.get("https://www.python.org")
 
